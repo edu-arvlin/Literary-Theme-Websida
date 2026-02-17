72c5744f9407983b50954d7d3e337e94d9c592a6
edu-arvlin <arvid.lind@skola.taby.se>
2026-02-17 14:29:05 +0100

    Grisch eller något

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function changeLanguage() {
         document.getElementById("text").textContent = "This is my website.";
         document.getElementById("button").textContent = "Svenska";
         Swedish = false;
-
+        console.log("Swedish is now false");
     } else if (Swedish === false) {
         document.getElementById("title").textContent = "Välkommen";
         document.getElementById("text").textContent = "Detta är min hemsida.";
         document.getElementById("button").textContent = "English";
         Swedish = true;
+        console.log("Swedish is now true");
     }
 }
