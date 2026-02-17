b4b22e84575c9a7020c597e00effd03bbf2632ab
edu-arvlin <arvid.lind@skola.taby.se>
2026-02-17 10:32:56 +0100

    Bytte variabler

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let Swedish = true;
 
 function changeLanguage() {
-
+    
     if (Swedish === true) {
         document.getElementById("title").textContent = "Welcome";
         document.getElementById("text").textContent = "This is my website.";
-        document.getElementById("btn").textContent = "Svenska";
+        document.getElementById("button").textContent = "Svenska";
         Swedish = false;
 
     } else if (Swedish === false) {
         document.getElementById("title").textContent = "Välkommen";
         document.getElementById("text").textContent = "Detta är min hemsida.";
-        document.getElementById("btn").textContent = "English";
+        document.getElementById("button").textContent = "English";
         Swedish = true;
     }
 }
