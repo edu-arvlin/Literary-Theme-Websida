dd567efbe06b721a82d7b57220c94803f4709e02
edu-arvlin <arvid.lind@skola.taby.se>
2026-02-18 11:12:27 +0100

    Fixade till saker

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let Swedish = true;
 
-function changeLanguage() {
-    
-    if (Swedish === true) {
-        document.getElementById("title").textContent = "Welcome";
-        document.getElementById("text").textContent = "This is my website.";
-        document.getElementById("button").textContent = "Svenska";
-        Swedish = false;
-        console.log("Swedish is now false");
-    } else if (Swedish === false) {
+let swedenButton = document.getElementById("SwedenButton");
+let ukButton = document.getElementById("UKButton");
+
+function updateText() {
+
+    if (Swedish) {
         document.getElementById("title").textContent = "Välkommen";
         document.getElementById("text").textContent = "Detta är min hemsida.";
         document.getElementById("button").textContent = "English";
-        Swedish = true;
-        console.log("Swedish is now true");
+    } 
+    else {
+        document.getElementById("title").textContent = "Welcome";
+        document.getElementById("text").textContent = "This is my website.";
+        document.getElementById("button").textContent = "Svenska";
     }
 }
+
+swedenButton.addEventListener("click", function() {
+    Swedish = true;
+    updateText();
+});
+
+ukButton.addEventListener("click", function() {
+    Swedish = false;
+    updateText();
+});
+
+
