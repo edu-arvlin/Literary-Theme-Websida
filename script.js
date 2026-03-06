2ce0f0bd04e33977131289888884086dae64e67b
edu-arvlin <arvid.lind@skola.taby.se>
2026-03-06 11:25:15 +0100

    Ny font typ allat
    Små ändringar också

--- script.js
+++ script.js
@@ -6,19 +6,16 @@ let ukButton = document.getElementById("UKButton");
 function updateText() {
 
     if (Swedish) {
-        document.getElementById("toptitle").textContent = "Etnicitet, Identitet & Neokolonialism i Born a Crime & Robinson Crusoe";
-        document.getElementById("title").textContent = "Välkommen till vår analys av Born a Crime av Trevor Noah, samt Robinson Crusoe av Daniel Defoe";
-        document.getElementById("text").textContent = "Born a Crime är en självbiografi av Trevor Noah om hans uppväxt i Sydafrika under apartheidens tid. Robinson Crusoe är en robinsonad av Daniel Defoe som handlar om hans överlednad på en nästintill öde ö efter ett skeppsbrott. Boda böckerna är starkt fokuserad på teman som etnicitet, identitet och neokolonialism, och vi kommer att analysera hur dessa teman presenteras i båda böckerna samt hur det ser ut idag. ";
-        console.log("Swedish is true, text updated to Swedish.");
+        document.getElementById("toptitle").textContent = "Etnicitet, Identitet & Neokolonialism i Born a Crime & Robinson Kruse";
+        document.getElementById("title").textContent = "Välkommen till vår analys av Born a Crime av Trevor Noah, samt Robinson Kruse av Daniel Defoe";
+        document.getElementById("text").textContent = "Born a Crime är en självbiografi av Trevor Noah om hans uppväxt i Sydafrika under apartheidens tid. Robinson Kruse är en robinsonad av Daniel Defoe som handlar om hans överlednad på en nästintill öde ö efter ett skeppsbrott. Båda böckerna är starkt fokuserade på teman som etnicitet, identitet och neokolonialism, vår websida kommer presentera hur dessa teman förekommer i båda böckerna samt hur det ser ut idag.";
     } 
     else {
         document.getElementById("toptitle").textContent = "Ethnicity, Identity & Neocolonialism in Born a Crime & Robinson Crusoe";
-        document.getElementById("title").textContent = "Welcome to our analysis of Born a Crime by Trevor Noah, as well as Robinson Crusoe by Daniel Defoe";
-        document.getElementById("text").textContent = "Born a Crime is a memoir by Trevor Noah about his childhood in South Africa during apartheid.";
-        console.log("Swedish is false, text updated to English.");
-    }
+        document.getElementById("title").textContent = "Welcome to our analysis of Born a Crime by Trevor Noah, as well as Robinson Kruse by Daniel Defoe";
+        document.getElementById("text").textContent = "Born a Crime is a memoir by Trevor Noah about his childhood in South Africa during the time of apartheid. Robinson Crusoe is a robinsonade by Daniel Defoe that tells the story of his survival on a nearly deserted island after a shipwreck. Both books are heavily focused on themes such as ethnicity, identity and neocolonialism, our website will present how these themes are present in both books as well as how it looks today.";
 }
-
+}
 swedenButton.onclick = function() {
     Swedish = true;
     updateText();
